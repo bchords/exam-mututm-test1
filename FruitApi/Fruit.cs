5ecb9dc25ded6a1c70fdfa148b07e5018381cb2b
bchords <34419165+bchords@users.noreply.github.com>
2025-08-03 14:31:06 +0100

    Delete FruitApi directory

--- FruitApi/Fruit.cs
+++
@@ -1,8 +0,0 @@
-public class Fruits
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Color { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public bool InStock { get; set; }
-}
