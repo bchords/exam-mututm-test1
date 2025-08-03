8ab5cc5db01d1ea32360af2d3cb6fa4e04b458f8
bchords <34419165+bchords@users.noreply.github.com>
2025-08-03 13:25:57 +0100

    Create Program.cs

---
+++ FruitApi/Program.cs
@@ -0,0 +1,35 @@
+// Program.cs
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Entity Framework
+builder.Services.AddDbContext<FruitDb>(options =>
+    options.UseInMemoryDatabase("FruitList"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+// Ensure database is created
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<FruitDb>();
+    dbContext.Database.EnsureCreated();
+}
+
+app.Run();
